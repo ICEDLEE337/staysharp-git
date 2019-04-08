3853c91bfe415919c9ca1f1029ebde05f9b2ec55
leenorris <lee.norris@philips.com>
2019-04-08 08:38:32 -0400

    "fixed" router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,60 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Route, Routes} from '@angular/router';
-import * as _ from 'lodash';
+import {RouterModule, Routes} from '@angular/router';
 import {PrinciplesComponent} from './entry-components/principles/principles.component';
 import {NotFoundComponent} from './entry-components/not-found/not-found.component';
 import {ScenariosComponent} from './entry-components/scenarios/scenarios.component';
 import {ShellComponent} from './entry-components/shell/shell.component';
 
-import {componentRoute, defaultComponentRoute, redirectToComponent} from './app-routing-helpers';
+// import {componentRoute, defaultComponentRoute, redirectToComponent} from './app-routing-helpers';
 
-const enableTracing = true;
+const enableTracing = false;
+
+// const routes: Routes = [
+
+//   defaultComponentRoute(ShellComponent,
+//     [
+//       componentRoute(PrinciplesComponent),
+//       componentRoute(ScenariosComponent),
+//       redirectToComponent(ScenariosComponent),
+//     ]
+//   ),
+
+//   redirectToComponent(ShellComponent),
+
+//   {
+//     path: '**', component: NotFoundComponent
+//   }
+
+// ];
 
 const routes: Routes = [
-
-  defaultComponentRoute(ShellComponent,
-    [
-      componentRoute(PrinciplesComponent),
-      componentRoute(ScenariosComponent),
-      redirectToComponent(ScenariosComponent),
-    ]
-  ),
-
-  redirectToComponent(ShellComponent),
-
   {
-    path: '**', component: NotFoundComponent
+    "children": [
+      {
+        "path": "principles",
+        component: PrinciplesComponent
+      },
+      {
+        "path": "scenarios",
+        component: ScenariosComponent
+      },
+      {
+        "redirectTo": "scenarios",
+        "path": "",
+        "pathMatch": "full"
+      }
+    ],
+    "path": "",
+    component: ShellComponent
+  },
+  {
+    "redirectTo": "shell",
+    "path": "",
+    "pathMatch": "full"
+  },
+  {
+    "path": "**", component: NotFoundComponent
   }
-
 ];
 
 if (enableTracing) {
