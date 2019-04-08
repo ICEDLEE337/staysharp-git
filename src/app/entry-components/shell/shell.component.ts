9641b3461743c107fe97902c909728629747edb4
leenorris <lee.norris@philips.com>
2019-04-08 08:44:29 -0400

    still fixing routes

--- src/app/entry-components/shell/shell.component.ts
+++ src/app/entry-components/shell/shell.component.ts
@@ -1,24 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import {Route} from '@angular/router';
-import {componentRouteWithIcon} from 'src/app/app-routing-helpers';
-import {PrinciplesComponent} from '../principles/principles.component';
-import {ScenariosComponent} from '../scenarios/scenarios.component';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html'
 })
 export class ShellComponent implements OnInit {
-  routes: Route[];
+  routes: any[];
 
   constructor () {
-    this.routes = [
-      componentRouteWithIcon(PrinciplesComponent, 'vpn_key'),
-      componentRouteWithIcon(ScenariosComponent, 'public')
-    ];
   }
 
   ngOnInit () {
+    this.routes = [
+      {path: 'principles', icon: 'vpn_key'},
+      {path: 'principles', icon: 'public'},
+    ];
   }
 
 }
